4263f31116452cc6e6de3924d92563d0265bc955
Gerrad Andre <gerrad.andre@selu.edu>
2022-01-28 11:07:14 -0600

    383 Phase 2

---
+++ ProductsApp/Products_383/Features/ProductDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Products_383
+{
+    public class ProductDto
+    {
+        [Key]
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name")]
+        [MaxLength(120)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(1, int.MinValue, ErrorMessage = "Please enter a price greater than {1}")]
+        public decimal Price { get; set; }
+
+        public decimal? SalePrice  { get; set; }
+
+    }
+}
---
+++ ProductsApp/Products_383/Program.cs
@@ -0,0 +1,169 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var Products = new List<Products_383.ProductDto>();
+
+//CREATE
+app.MapPost("/api/Products/New", (Products_383.ProductDto product) =>
+{
+    var message = "Please put in a name and description";
+
+    if (CheckNull(product) != false)
+    {
+        var isVerified = Verification(product);
+        message = "Please Enter Name Less than 120 Characters and a Price greater than 0";
+
+        if (isVerified != false)
+        {
+            product.ProductId = UniqueId(Products);
+            Products.Add(product);
+            message = "Product created successfully";
+        }
+    }
+
+    
+    
+    return Results.Ok(message); 
+});
+
+//GET ALL
+app.MapGet("/api/Products/List", () =>
+{
+    var OnSale = Products.Where(x => x.SalePrice != 0 && x.SalePrice < x.Price).ToList();
+    return OnSale;
+})
+.WithName("GetProductList");
+
+// GET ONE
+app.MapGet("/api/Products/Item/{id}", (int id) =>
+{
+    var product = Products.FirstOrDefault(x => x.ProductId == id);
+    if (Products.FirstOrDefault(x => x.ProductId == id) is null)
+    {
+        return Results.NotFound();
+    }
+    else
+        return Results.Ok(product);
+});
+
+// EDIT
+app.MapPut("/api/Producsts/Edit/{id}", (int id, Products_383.ProductDto updatedProduct) =>
+{
+    //var Oldproduct = Products.FirstOrDefault(x => x.ProductId == id);
+    //var ValidProducts = Products.Where(x => x.ProductId == id);
+
+    if (Products.FirstOrDefault(x => x.ProductId == id) is null)
+    {
+        return Results.NotFound();
+    }
+    else
+        foreach (Products_383.ProductDto product in Products)
+        {
+            if(product.ProductId == id)
+            {
+                product.ProductId = id;
+                product.Name = updatedProduct.Name;
+                product.Description = updatedProduct.Description;
+                product.Price = updatedProduct.Price; 
+                product.SalePrice = updatedProduct.SalePrice;
+
+                var oldProduct = product;
+            }
+        }
+
+    return Results.Ok("Product Updated..... Hopefully");
+});
+
+// DELETE
+app.MapDelete("/api/Producsts/Delete/{id}", (int id) =>
+{
+    if (Products.FirstOrDefault(x => x.ProductId == id) is null)
+    {
+        return Results.NotFound();
+    }
+    else
+        Products.RemoveAll(x => x.ProductId == id);
+        return Results.Ok("Product Deleted..... Hopefully");
+});
+
+app.Run();
+
+static bool Verification(Products_383.ProductDto product)
+{
+    var isVerified = false;
+
+    if (product.Name.Length > 120 || product.Name == "")
+    {        
+        return isVerified;
+    }
+    else if (product.Price <= 0)
+    {
+        return isVerified;
+    }
+    else
+    {
+        isVerified = true;
+    }
+
+    return isVerified;
+}
+
+static bool CheckNull(Products_383.ProductDto product)
+{
+    var isNulled = false;
+
+    if (product.Name == null || product.Description == null)
+    {
+        return isNulled;
+    }
+    else
+    {
+        isNulled = true;
+    }
+
+    return isNulled;
+}
+
+static int UniqueId(List<Products_383.ProductDto> products)
+{
+    var id = 1;
+    foreach (var product in products)
+    {
+        if(products.Count == 0)
+        {
+            return id;
+        }
+        else
+        {
+            id++;
+        }                  
+    }
+
+    return id;
+}
+
+//public class ProductDto
+//{
+//    public int ProductId { get; set; }
+//    public string Name { get; set; }
+//    public string Description { get; set; }
+//    public decimal Price { get; set; }
+//    public decimal? SalePrice { get; set; }
+
+//}
+
